0eadf74e23253915392ee66347dd70ae4b72010b
choisangmin_git <choisangmin1112@gmail.com>
2025-07-14 13:13:30 +0900

    UPDATE 신청 사유 제외

--- src/main/java/com/example/moyeorak/dto/RentalApplicationRequest.java
+++ src/main/java/com/example/moyeorak/dto/RentalApplicationRequest.java
@@ -28,10 +28,6 @@ public class RentalApplicationRequest {
     @NotNull(message = "종료 시간은 필수입니다.")
     private LocalTime requestedEndTime;
 
-    @NotBlank(message = "신청 사유를 입력해주세요.")
-    @Size(max = 500, message = "신청 사유는 500자 이하로 입력해야 합니다.")
-    private String note;
-
     @NotNull(message = "신청 인원은 필수입니다.")
     @Min(value = 1, message = "신청 인원은 최소 1명 이상이어야 합니다.")
     private Integer peopleCount;
